64daebde54fb98e2a904e2e29097f525c1129889
Sviridoff_E <Sviridoff_E@outlook.com>
2018-09-05 22:43:44 +0300

    Improved UnexpectedTokenException

--- src/com/evg/sjl/exceptions/SyntaxExceptions.kt
+++ src/com/evg/sjl/exceptions/SyntaxExceptions.kt
@@ -5,9 +5,9 @@ import com.evg.sjl.lexer.TokenTypes
 
 abstract class SyntaxException(message: String) : SJLException(message)
 
-class UnexpectedTokenException(got: Token, expected: TokenTypes? = null)
+class UnexpectedTokenException(got: Token, vararg expected: TokenTypes? = arrayOf())
     : SyntaxException(
-        if (expected == null)
+        if (expected.isEmpty())
             "Unexpected token ${got.type} at ${got.position}"
-        else "Unexpected token ${got.type} at ${got.position}: expected $expected"
+        else "Unexpected token ${got.type} at ${got.position}: expected ${expected.joinToString(" or ")}"
 )
