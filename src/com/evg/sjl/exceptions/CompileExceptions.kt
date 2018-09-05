c379a16e23be63cc6be087745abc0f78205dbbf2
Sviridoff_E <Sviridoff_E@outlook.com>
2018-09-05 22:46:58 +0300

    Added InvalidOperandTypesException

--- src/com/evg/sjl/exceptions/CompileExceptions.kt
+++ src/com/evg/sjl/exceptions/CompileExceptions.kt
@@ -1,7 +1,13 @@
 package com.evg.sjl.exceptions
 
+import com.evg.sjl.lib.Operations
+import com.evg.sjl.values.Types
+
 abstract class CompileException(message: String)
     : SJLException(message)
 
 class VariableUsedWithoutBeingDeclaredException(identifier: String)
     : CompileException("Variable $$identifier used without being declared")
+
+class InvalidOperandTypesException(operation: Operations, vararg operandTypes: Types)
+    : CompileException("Unable to perform operation $operation with following operand types: ${operandTypes.joinToString(", ")}")
