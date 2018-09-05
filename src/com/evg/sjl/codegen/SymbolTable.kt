b3379a176c7f2c1c4360a34015c45f9ba2427873
Sviridoff_E <Sviridoff_E@outlook.com>
2018-09-05 22:40:09 +0300

    Symbol table now contains symbol types

--- src/com/evg/sjl/codegen/SymbolTable.kt
+++ src/com/evg/sjl/codegen/SymbolTable.kt
@@ -1,12 +1,24 @@
 package com.evg.sjl.codegen
 
+import com.evg.sjl.values.Types
+
 class SymbolTable {
-    val symbols = LinkedHashMap<String, Int>()
-    fun register(identifier: String): Int {
-        val value = if (symbols.isEmpty()) 1 else symbols.values.last() + 2 // +2 because of double type
-        symbols[identifier] = value
-        return value
+    val symbols = LinkedHashMap<String, Symbol>()
+    fun register(identifier: String, type: Types): Symbol {
+        if (identifier !in symbols) {
+            val index = when {
+                symbols.isEmpty() -> 1
+                else -> symbols.values.last().index +
+                        if (symbols.values.last().type == Types.NUMBER)
+                            2
+                        else 1
+            }
+            symbols[identifier] = Symbol(index, type)
+        }
+        return get(identifier)!!
     }
 
-    operator fun get(identifier: String) = symbols[identifier]
+    operator fun get(identifier: String): Symbol? = symbols[identifier]
 }
+
+data class Symbol(val index: Int, val type: Types)
