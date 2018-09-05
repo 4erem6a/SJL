4c3bec9eecb62aebd62cc84fd37d7699859b6182
Sviridoff_E <Sviridoff_E@outlook.com>
2018-09-05 22:38:28 +0300

    Replace NumberExpression with ValueExpression

--- src/com/evg/sjl/parser/ast/AST.kt
+++ src/com/evg/sjl/parser/ast/AST.kt
@@ -4,6 +4,7 @@ import com.evg.sjl.lib.BinaryOperations
 import com.evg.sjl.lib.UnaryOperations
 import com.evg.sjl.parser.visitors.Visitor
 import com.evg.sjl.values.Types
+import com.evg.sjl.values.Value
 
 interface Node {
     fun accept(visitor: Visitor)
@@ -33,21 +34,18 @@ data class UnionStatement(val statements: List<Statement>) : Statement {
 
 interface Expression : Node
 
-data class NumberExpression(val number: Double) : Expression {
+data class ValueExpression(val value: Value) : Expression {
     override fun accept(visitor: Visitor) = visitor.visit(this)
-
 }
 
 data class BinaryExpression(val operation: BinaryOperations,
                             val left: Expression, val right: Expression) : Expression {
     override fun accept(visitor: Visitor) = visitor.visit(this)
-
 }
 
 data class UnaryExpression(val operation: UnaryOperations,
                            val expression: Expression) : Expression {
     override fun accept(visitor: Visitor) = visitor.visit(this)
-
 }
 
 data class VariableExpression(val identifier: String) : Expression {
