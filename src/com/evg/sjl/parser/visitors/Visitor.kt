1af1fbb7f7fbab976307b0d90603d1d6687e647f
Sviridoff_E <Sviridoff_E@outlook.com>
2018-09-05 22:41:17 +0300

    Add default implementation for Visitor interface

--- src/com/evg/sjl/parser/visitors/Visitor.kt
+++ src/com/evg/sjl/parser/visitors/Visitor.kt
@@ -3,15 +3,37 @@ package com.evg.sjl.parser.visitors
 import com.evg.sjl.parser.ast.*
 
 interface Visitor {
-    fun visit(statement: ExpressionStatement)
-    fun visit(statement: PrintStatement)
-    fun visit(statement: UnionStatement)
-    fun visit(statement: VariableDefinitionStatement)
-    fun visit(statement: AssignmentStatement)
+    fun visit(statement: ExpressionStatement) {
+        statement.expression.accept(this)
+    }
 
-    fun visit(expression: NumberExpression)
-    fun visit(expression: BinaryExpression)
-    fun visit(expression: UnaryExpression)
-    fun visit(expression: VariableExpression)
-    fun visit(expression: InputExpression)
+    fun visit(statement: PrintStatement) {
+        statement.expression.accept(this)
+    }
+
+    fun visit(statement: UnionStatement) {
+        statement.statements.forEach { it.accept(this) }
+    }
+
+    fun visit(statement: VariableDefinitionStatement) {}
+
+    fun visit(statement: AssignmentStatement) {
+        statement.expression.accept(this)
+    }
+
+
+    fun visit(expression: ValueExpression) {}
+
+    fun visit(expression: BinaryExpression) {
+        expression.left.accept(this)
+        expression.right.accept(this)
+    }
+
+    fun visit(expression: UnaryExpression) {
+        expression.expression.accept(this)
+    }
+
+    fun visit(expression: VariableExpression) {}
+
+    fun visit(expression: InputExpression) {}
 }
