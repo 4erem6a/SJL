c15a3dcce80f231c79e73cc4f03c52db7f95163d
Sviridoff_E <Sviridoff_E@outlook.com>
2018-09-16 16:40:49 +0300

    Added bitwise negation compilation support.

--- src/com/evg/sjl/parser/ast/UnaryExpression.kt
+++ src/com/evg/sjl/parser/ast/UnaryExpression.kt
@@ -5,7 +5,7 @@ import com.evg.sjl.exceptions.InvalidOperandTypesException
 import com.evg.sjl.lib.UnaryOperations
 import com.evg.sjl.parser.visitors.Visitor
 import com.evg.sjl.values.Types
-import jdk.internal.org.objectweb.asm.Opcodes
+import jdk.internal.org.objectweb.asm.Opcodes.*
 import jdk.internal.org.objectweb.asm.tree.InsnNode
 
 class UnaryExpression(val operation: UnaryOperations,
@@ -14,11 +14,16 @@ class UnaryExpression(val operation: UnaryOperations,
         expression.compile(context)
         val type = context.typeInference.getType(expression)
         when (type) {
-            Types.DOUBLE -> when (expression) {
-                UnaryOperations.NEGATION -> context.il.add(InsnNode(Opcodes.DNEG))
+            Types.DOUBLE -> when (operation) {
+                UnaryOperations.NEGATION -> context.il.add(InsnNode(DNEG))
+                else -> throw InvalidOperandTypesException(operation, type)
             }
-            Types.INTEGER -> when (expression) {
-                UnaryOperations.NEGATION -> context.il.add(InsnNode(Opcodes.INEG))
+            Types.INTEGER -> when (operation) {
+                UnaryOperations.NEGATION -> context.il.add(InsnNode(INEG))
+                UnaryOperations.BITWISE_NEGATION -> {
+                    context.il.add(InsnNode(ICONST_M1))
+                    context.il.add(InsnNode(IXOR))
+                }
             }
             else -> throw InvalidOperandTypesException(operation, type)
         }
