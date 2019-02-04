4d30f225d13dd1e84197da2c482fd7dee31861b3
4erem6a <Sviridoff_E@mail.ru>
2019-02-05 02:47:18 +0300

    Add initializer expression support to VariableDefinitionStatement

--- src/com/evg/sjl/parser/ast/VariableDefinitionStatement.kt
+++ src/com/evg/sjl/parser/ast/VariableDefinitionStatement.kt
@@ -1,28 +1,41 @@
 package com.evg.sjl.parser.ast
 
 import com.evg.sjl.codegen.CompilationContext
+import com.evg.sjl.exceptions.TypeInferenceFailException
+import com.evg.sjl.exceptions.VariableAlreadyDeclaredException
 import com.evg.sjl.parser.visitors.Visitor
 import com.evg.sjl.values.Types
 import jdk.internal.org.objectweb.asm.Opcodes
 import jdk.internal.org.objectweb.asm.tree.LdcInsnNode
 import jdk.internal.org.objectweb.asm.tree.VarInsnNode
 
-class VariableDefinitionStatement(var identifier: String, var type: Types) : Statement {
+class VariableDefinitionStatement(
+        var identifier: String,
+        var type: Types? = null,
+        var initializer: Expression? = null
+) : Statement {
     override fun compile(context: CompilationContext) {
+        val iniType = if (initializer != null)
+            context.typeInference.getType(initializer!!)
+        else null
+        if (type != null && iniType != null)
+            if (type != iniType)
+                initializer = CastExpression(type!!, initializer!!)
+        if (type == null && iniType == null)
+            throw TypeInferenceFailException()
+        val type = type ?: iniType!!
+        if (context.symbolTable.getTop(identifier) != null)
+            throw VariableAlreadyDeclaredException(identifier)
         val symbol = context.symbolTable.register(identifier, type)
+        initializer?.compile(context)
+                ?: context.il.add(LdcInsnNode(type.defaultValue))
         when (type) {
-            Types.DOUBLE -> {
-                context.il.add(LdcInsnNode(type.defaultValue))
+            Types.DOUBLE ->
                 context.il.add(VarInsnNode(Opcodes.DSTORE, symbol.index))
-            }
-            Types.INTEGER, Types.BOOLEAN -> {
-                context.il.add(LdcInsnNode(type.defaultValue))
+            Types.INTEGER, Types.BOOLEAN ->
                 context.il.add(VarInsnNode(Opcodes.ISTORE, symbol.index))
-            }
-            Types.STRING -> {
-                context.il.add(LdcInsnNode(type.defaultValue))
+            Types.STRING ->
                 context.il.add(VarInsnNode(Opcodes.ASTORE, symbol.index))
-            }
         }
     }
 
