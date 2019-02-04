ca5e0a68e796939e02f6c11a793ca1328d6ef67b
4erem6a <Sviridoff_E@mail.ru>
2019-02-05 01:46:53 +0300

    Add scope support to symbol table

--- src/com/evg/sjl/codegen/SymbolTable.kt
+++ src/com/evg/sjl/codegen/SymbolTable.kt
@@ -3,22 +3,41 @@ package com.evg.sjl.codegen
 import com.evg.sjl.values.Types
 
 class SymbolTable {
-    val symbols = LinkedHashMap<String, Symbol>()
-    fun register(identifier: String, type: Types): Symbol {
-        if (identifier !in symbols) {
-            val index = when {
-                symbols.isEmpty() -> 1
-                else -> symbols.values.last().index +
-                        if (symbols.values.last().type == Types.DOUBLE)
-                            2
-                        else 1
-            }
-            symbols[identifier] = Symbol(index, type)
-        }
-        return get(identifier)!!
+    class SymbolScope(var subscope: SymbolScope? = null) {
+        val symbols = HashMap<String, Symbol>()
+
+        fun get(identifier: String): Symbol?
+            = symbols[identifier] ?: subscope?.get(identifier)
     }
 
-    operator fun get(identifier: String): Symbol? = symbols[identifier]
+    var topScope = SymbolScope()
+    private var lastSymbol: Symbol = Symbol(0, Types.INTEGER)
+
+    fun register(identifier: String, type: Types): Symbol {
+        val existing = topScope.symbols[identifier]
+        if (existing != null)
+            return existing
+        val index = lastSymbol.index + when (lastSymbol.type) {
+            Types.DOUBLE -> 2
+            else -> 1
+        }
+        val symbol = Symbol(index, type)
+        topScope.symbols[identifier] = symbol
+        lastSymbol = symbol
+        return symbol
+    }
+
+    fun upScope() {
+        topScope = SymbolScope(topScope)
+    }
+
+    fun downScope() {
+        topScope = topScope.subscope ?: topScope
+    }
+
+    operator fun get(identifier: String): Symbol? = topScope.get(identifier)
+
+    fun getTop(identifier: String): Symbol? = topScope.symbols[identifier]
 }
 
 data class Symbol(val index: Int, val type: Types)
