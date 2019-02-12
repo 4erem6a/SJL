093d345ff93acdec3313f8b5e690656daad9553a
4erem6a <Sviridoff_E@mail.ru>
2019-02-12 11:30:58 +0300

    Update ExpressionStatement

--- src/com/evg/sjl/parser/ast/ExpressionStatement.kt
+++ src/com/evg/sjl/parser/ast/ExpressionStatement.kt
@@ -2,10 +2,19 @@ package com.evg.sjl.parser.ast
 
 import com.evg.sjl.codegen.CompilationContext
 import com.evg.sjl.parser.visitors.Visitor
+import com.evg.sjl.values.Types
+import jdk.internal.org.objectweb.asm.Opcodes
+import jdk.internal.org.objectweb.asm.Opcodes.*
+import jdk.internal.org.objectweb.asm.tree.InsnNode
 
 class ExpressionStatement(var expression: Expression) : Statement {
     override fun compile(context: CompilationContext) {
+        val type = context.typeInference.getType(expression)
         expression.compile(context)
+        context.il.add(InsnNode(when (type) {
+            Types.DOUBLE -> POP2
+            else -> POP
+        }))
     }
 
     override fun accept(visitor: Visitor) = visitor.visit(this)
