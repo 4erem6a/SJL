d4a285a9ba75fd6196ef3619d30ffc2f9536bc9b
4erem6a <Sviridoff_E@mail.ru>
2019-04-02 18:59:01 +0300

    Add automatic array type inference in variable definitions

--- src/com/evg/sjl/ast/VariableDefinitionStatement.kt
+++ src/com/evg/sjl/ast/VariableDefinitionStatement.kt
@@ -1,42 +1,41 @@
 package com.evg.sjl.ast
 
+import com.evg.sjl.ast.visitors.Visitor
 import com.evg.sjl.codegen.CompilationContext
 import com.evg.sjl.exceptions.InvalidValueTypeException
 import com.evg.sjl.exceptions.MissingInitializerException
 import com.evg.sjl.exceptions.TypeInferenceFailException
 import com.evg.sjl.exceptions.VariableAlreadyDeclaredException
-import com.evg.sjl.ast.visitors.Visitor
-import com.evg.sjl.values.Primitives
-import com.evg.sjl.values.Referential
-import com.evg.sjl.values.StringType
-import com.evg.sjl.values.Type
+import com.evg.sjl.values.*
 import jdk.internal.org.objectweb.asm.Opcodes
 import jdk.internal.org.objectweb.asm.tree.VarInsnNode
 
 class VariableDefinitionStatement(
-        var identifier: String,
-        var type: Type? = null,
-        var initializer: Expression? = null
+        val identifier: String,
+        val type: Type? = null,
+        val initializer: Expression? = null
 ) : Statement {
     override fun compile(context: CompilationContext) {
-        val iniType = if (initializer != null)
-            context.typeInference.getType(initializer!!)
+        val ini = if (initializer is ArrayExpression
+                && initializer.type == null
+                && initializer.values.isEmpty()
+                && type is ArrayType)
+            ArrayExpression(type.type, initializer.length)
+        else initializer
+        val iniType = if (ini != null)
+            context.typeInference.getType(ini)
         else null
-        if (type != null && iniType != null)
-            if (type != iniType)
-                initializer = CastExpression(type!!, initializer!!)
         if (type == null && iniType == null)
             throw TypeInferenceFailException()
         val type = type ?: iniType!!
         if (context.symbolTable.getTop(identifier) != null)
             throw VariableAlreadyDeclaredException(identifier)
         val symbol = context.symbolTable.register(identifier, type)
-        initializer?.compile(context)
-                ?: if (type is Primitives)
-                    ValueExpression(type.defaultValue).compile(context)
-                else if (type is StringType)
-                    ValueExpression(StringType.defaultValue).compile(context)
-                else throw MissingInitializerException(identifier)
+        ini?.compile(context) ?: when (type) {
+            is Primitives -> ValueExpression(type.defaultValue).compile(context)
+            is StringType -> ValueExpression(StringType.defaultValue).compile(context)
+            else -> throw MissingInitializerException(identifier)
+        }
         when (type) {
             Primitives.DOUBLE ->
                 context.il.add(VarInsnNode(Opcodes.DSTORE, symbol.index))
